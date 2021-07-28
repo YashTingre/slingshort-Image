614ef710847e8435e298a77c7250e227def5167b
YashTingre <85239029+YashTingre@users.noreply.github.com>
2021-07-28 19:56:49 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,35 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.sling1,210,23);
+        image(this.sling2,180,20);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y-50);
+            line(pointA.x, pointA.y, pointB.x+35, pointB.y-40);
+            image(this.sling3,pointA.x,pointA.y,20,10);
+        }
+    }
+    
+}
